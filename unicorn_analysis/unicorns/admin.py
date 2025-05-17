f6879470240c231a87e05dfbbecaba6cf79a4152
Masaru124 <masarukaze041@gmail.com>
2025-05-17 20:33:46 +0530

    added admin

--- unicorn_analysis/unicorns/admin.py
+++ unicorn_analysis/unicorns/admin.py
@@ -6,3 +6,15 @@ class UnicornCompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'valuation', 'industry', 'country', 'founded_year')
     search_fields = ('name', 'industry', 'country')
     list_filter = ('industry', 'country', 'founded_year')
+    ordering = ('name', 'founded_year')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'valuation', 'industry', 'country', 'city', 'founded_year')
+        }),
+        ('Financial Information', {
+            'fields': ('total_raised', 'financial_stage', 'investors_count', 'deal_terms', 'portfolio_exits')
+        }),
+        ('Additional Information', {
+            'fields': ('date_joined', 'investors', 'description')
+        }),
+    )
